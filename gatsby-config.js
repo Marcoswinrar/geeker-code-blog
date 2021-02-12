7e819683a210edf0d96eb3c92f8ec17f30eacd63
marcos.winiarski <marcos.winiarski@ambevtech.com.br>
2021-02-12 19:50:14 -0300

    Limpando estrutura inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Geeker Code`,
+    description: `Um blog geek sobre rotina, desenvolvimento e quem sabe educação financeira.`,
+    author: `Marcos Eduardo Winiarski`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
