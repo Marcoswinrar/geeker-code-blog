d787d9adc43a4640d48804c80c760593c21fa2a5
marcos.winiarski <marcos.winiarski@ambevtech.com.br>
2021-02-21 22:48:30 -0300

    Ajuste de config para template do CMS e coreção na página de about

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,27 +12,30 @@ const AboutPage = () => (
     <MainContent>
       <h1> Geeker Code Blog </h1>
       <p>
-        Olá, me chamo Marcos Eduardo Winiarski, sou do interior do Paraná de uma cidade
-        chamada Capitão Leônidas Marques, pois é acho que ninguem conhece...
+        Olá, me chamo Marcos Eduardo Winiarski, sou do sertão do Paraná e moro em uma cidade muito louca
+        chamada Capitão Leônidas Marques. Alias acho que ninguém deve conhecer né ? (não pesquisem!!!)
       </p>
       <p>
         Atualmente sou desenvolvedor na{' '}
         <a
+          style={{color: "#c02942"}}
           href="https://ambevtech.com.br/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ambev Tech (#tamojunto)
+          Ambev Tech
       </a>
-      , além disso curto muito desenvolvimento, jogos e leitura.
-
-      Sou formado em TADS(Analise de Sistemas) desde 2018. Apaixonado
-      por web, especialmente front-end é claro (kkkk). Acho que posso ser livre
-      e aprender mais fácilmente. Adoro estudar e tentar ajudar os outros.
+      #tamojunto.
+      </p>
+      <p>
+        Formado em TADS(Analise de Sistemas) desde 2018. Apaixonado
+        por web, especialmente front-end é claro. Gosto de desenvolver
+        pois sinto no desenvolvimento a liberdade de criar coisas jamais pensadas de uma
+        forma divertida.
       </p>
       <p>
         É justamente por isso que este blog nasceu, com o intuíto de compartilhar
-        conhecimento e de cada vez aprender mais.
+        conhecimento e de aprender cada vez mais. Espero que gostem !
       </p>
       <h2>Contato</h2>
       <p>
